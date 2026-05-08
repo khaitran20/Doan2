0d44d9ac0083811339c7816729d7f06dfe95194c
KhaiTran <23004228@st.vlute.edu.vn>
2026-05-09 01:09:08 +0700

    thêm file kết nối

--- ketnoi.php
+++ ketnoi.php
@@ -1,24 +1,15 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+$host = 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com';
+$port = '4000';
+$user = '3teo7ak1vNGCXsh.root';
+$pass = 'Ex4avdggPFWM0bWH'; // Mật khẩu trong ảnh của bạn
+$dbname = 'test'; // Mặc định TiDB có sẵn db 'test', bạn nên dùng db này cho nhanh
 
-// 1. Khai báo thông tin kết nối từ image_847a0b.png 📋
-$host = "localhost";
-$user = "lkzafhlb_baixe"; 
-$pass = "QWTRxA5XNtCHLmcS95Cp"; // Mật khẩu chính xác từ ảnh 🔑
-$db   = "lkzafhlb_baixe"; 
+// Kết nối có thêm tham số Port
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
 
-// 2. Thiết lập kết nối 🔌
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-// 3. Kiểm tra kết nối 🛠️
 if (!$conn) {
     die("Kết nối thất bại: " . mysqli_connect_error());
 }
-
-// 4. Cấu hình font chữ hiển thị tiếng Việt 🇻🇳
-mysqli_set_charset($conn, 'utf8');
-
-
+echo "Kết nối CSDL thành công!";
 ?>
